7aebf20c1face0fb26131300e962f2aafcc20a8e
mwises <88902469+mwises@users.noreply.github.com>
2023-10-10 14:34:37 -0300

    styled-components

---
+++ styled.d.ts
@@ -0,0 +1,41 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primaryColor: string;
+      secondaryColor: string;
+      white: string;
+      mediumGray: string;
+    };
+    font: {
+      family: {
+        default: string;
+        secondary: string;
+      };
+      sizes: {
+        xsmall: string;
+        small: string;
+        medium: string;
+        large: string;
+        xlarge: string;
+        xxlarge: string;
+        huge: string;
+        xhuge: string;
+      };
+    };
+    media: {
+      lteMedium: string;
+    };
+    spacings: {
+      xsmall: string;
+      small: string;
+      medium: string;
+      large: string;
+      xlarge: string;
+      xxlarge: string;
+      huge: string;
+      xhuge: string;
+    };
+  }
+}
