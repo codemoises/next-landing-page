292e03e4b53ac997d3cbcd709c58697906f2b123
mwises <88902469+mwises@users.noreply.github.com>
2023-10-11 12:19:16 -0300

    getStaticProps

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import P from 'prop-types';
+import config from '../config';
+import { mapData } from '../api/map-data';
+import Home from '../templates/Home';
+
+export default function Index({ data = null }) {
+  return <Home data={data} />;
+}
+
+export const getStaticProps = async () => {
+  const raw = await fetch(config.url + config.defaultSlug);
+  const json = await raw.json();
+  const data = mapData(json);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+Index.propTypes = {
+  data: P.array,
+};
--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -1,43 +1,19 @@
+import P from 'prop-types';
+import Head from 'next/head';
 import { Base } from '../Base';
-import { useEffect, useState } from 'react';
 import { PageNotFound } from '../PageNotFound';
-import { Loading } from '../Loading';
 import { GridTwoColumn } from '../../components/GridTwoColumn';
 import { GridContent } from '../../components/GridContent';
 import { GridText } from '../../components/GridText';
 import { GridImage } from '../../components/GridImage';
-import { useLocation } from 'react-router-dom';
 import config from '../../config';
 
-function Home() {
-  const [data] = useState([]);
-  const location = useLocation();
-
-  useEffect(() => {}, [location]);
-
-  useEffect(() => {
-    if (data === undefined) {
-      document.title = `Página não encontrada | ${config.siteName}`;
-    }
-
-    if (data && !data.slug) {
-      document.title = `Carregando... | ${config.siteName}`;
-    }
-
-    if (data && data.title) {
-      document.title = `${data.title} | ${config.siteName}`;
-    }
-  }, [data]);
-
-  if (data === undefined) {
+function Home({ data }) {
+  if (!data || !data.length) {
     return <PageNotFound />;
   }
 
-  if (data && !data.slug) {
-    return <Loading />;
-  }
-
-  const { menu, sections, footerHtml, slug } = data;
+  const { menu, sections, footerHtml, slug, title } = data[0];
   const { links, text, link, srcImg } = menu;
 
   return (
@@ -46,22 +22,23 @@ function Home() {
       footerHtml={footerHtml}
       logoData={{ text, link, srcImg }}
     >
+      <Head>
+        <title>
+          {title} | {config.siteName}
+        </title>
+      </Head>
       {sections.map((section, index) => {
         const { componet } = section;
         const key = `${slug}-${index}`;
-
         if (componet === 'section.section-two-columns') {
           return <GridTwoColumn key={key} {...section} />;
         }
-
         if (componet === 'section.section-content') {
           return <GridContent key={key} {...section} />;
         }
-
         if (componet === 'section.section-grid-text') {
           return <GridText key={key} {...section} />;
         }
-
         if (componet === 'section.section-grid-image') {
           return <GridImage key={key} {...section} />;
         }
@@ -71,3 +48,7 @@ function Home() {
 }
 
 export default Home;
+
+Home.propTypes = {
+  data: P.array,
+};
