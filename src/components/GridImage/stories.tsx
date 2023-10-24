69e3be9454ec51d1a5808b2017e87c41ecac714a
mwises <88902469+mwises@users.noreply.github.com>
2023-10-24 10:36:30 -0300

    Update stories.tsx

--- src/components/GridImage/stories.tsx
+++ src/components/GridImage/stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, Story } from '@storybook/react/types-6-0';
+import { Meta, Story } from '@storybook/react';
 import { GridImage, GridImageProps } from '.';
 
 import mock from './mock';
